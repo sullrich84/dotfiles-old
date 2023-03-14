19b379e4e96f7058c96f630da99445af23760a29
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2023-03-14 15:10:31 +0100

    feat: some customizations

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "bash",
+        -- "bash",
         "help",
         "html",
         "javascript",
